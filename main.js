f6bca00bfdcfbbde83341ade044e60054bd7e3c4
krzyspazdzior <1p22pazdzior@gmail.com>
2024-04-11 13:47:43 +0200

    hamburger, scaling form, working js effects

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//shadow po scrollu na navie
+
 window.addEventListener('scroll', function() {
     const nav = document.querySelector('nav');
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
@@ -7,4 +9,37 @@ window.addEventListener('scroll', function() {
     } else {
         nav.classList.remove('nav-scroll-shadow');
     }
+});
+
+//hamburger open/close
+
+const mobileLinks = document.querySelectorAll('.mobile-link');
+const hamburger = document.querySelector('.hamburger');
+
+
+// function hamburgerToX(x) {
+//     x.classList.toggle("change");
+//     document.querySelector('.mobile-menu').classList.toggle('active');
+//     document.querySelector('nav').classList.toggle('active-nav');
+//     document.querySelector('nav').style.transition = 'all 0.1s ease';
+//   }
+  
+// mobileLink.forEach( link =>{
+//     link.addEventListener('click', function(){
+//         hamburger.classList.toggle("change");
+//         document.querySelector('.mobile-menu').classList.toggle('active');
+//     });
+// })
+
+function toggleMenu() {
+    hamburger.classList.toggle("change");
+    document.querySelector('.mobile-menu').classList.toggle('active');
+    document.querySelector('nav').classList.toggle('active-nav');
+    document.querySelector('nav').style.transition = 'all 0.1s ease';
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+mobileLinks.forEach(link => {
+    link.addEventListener('click', toggleMenu);
 });

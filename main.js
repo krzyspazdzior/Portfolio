47b629166d584e04358025c164c5527218bad55b
krzyspazdzior <1p22pazdzior@gmail.com>
2024-04-24 14:43:27 +0200

    Gallery hover + grid working

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//shadow po scrollu na navie
+// shadow po scrollu na navie
 
 window.addEventListener('scroll', function() {
     const nav = document.querySelector('nav');
@@ -11,7 +11,7 @@ window.addEventListener('scroll', function() {
     }
 });
 
-//hamburger open/close
+// hamburger open/close
 
 const mobileLinks = document.querySelectorAll('.mobile-link');
 const hamburger = document.querySelector('.hamburger');
@@ -28,4 +28,25 @@ hamburger.addEventListener('click', toggleMenu);
 
 mobileLinks.forEach(link => {
     link.addEventListener('click', toggleMenu);
+});
+
+
+
+const galleryCards = document.querySelectorAll(".gallery-card");
+
+galleryCards.forEach((card) => {
+    const btn = card.querySelector(".gallery-btn");
+    const img = card.querySelector(".gallery-img");
+
+    // Mouseover event
+    card.addEventListener('mouseover', function() {
+        if (btn) btn.style.display = "block";
+        if (img) img.style.filter = "blur(4px)";
+    });
+
+    // Mouseout event
+    card.addEventListener('mouseout', function() {
+        if (btn) btn.style.display = "none";
+        if (img) img.style.filter = "blur(0)";
+    });
 });
